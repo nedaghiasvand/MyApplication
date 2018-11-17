36294673928f7360d654db0b6c8fb8271207a57b
neda ghiasvand <neda73ghiasvand@gmail.com>
2018-11-17 22:48:56 +0330

    git init

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,12 @@
+package com.example.lib;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.print("hello");
+    }
+
+
+//    public void run() {
+//
+//    }
+}
